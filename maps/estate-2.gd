298cc9d88baca94ab542b531e98f79b6ef28810a
Thomas Holmes <thomasholmes@outlook.com>
2023-12-09 12:37:47 +0000

    Added collision to house tiles.

--- maps/estate-2.gd
+++ maps/estate-2.gd
@@ -1,5 +1,5 @@
 extends Node2D
 
 func move_to_spawn_point(body: RigidBody2D):
-	body.position = Vector2(-20.5 * 64, 6.5 * 64)
+	body.position = Vector2(-20.5 * 64, 7.5 * 64)
 	body.rotation_degrees = -90
