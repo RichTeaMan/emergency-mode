d2999401b51dae43c4e78fa0bc7cefd56eb69efe
Thomas Holmes <thomasholmes@outlook.com>
2023-12-09 14:15:01 +0000

    Build date now is iso format.

--- setBuildConstants.sh
+++ setBuildConstants.sh
@@ -2,10 +2,10 @@
 
 
 COMMIT_HASH=$(git rev-parse --short HEAD)
-BUILD_DATE=$(date)
+BUILD_DATE=$(date -u --iso-8601=minutes | sed s/+00:00/Z/)
 
 echo "build hash: $COMMIT_HASH"
-git show
+echo "build date: $BUILD_DATE"
 
 sed -i "s/var commit_hash = \"dev\"/var commit_hash = \"$COMMIT_HASH\"/" singletons/build_info.gd
 sed -i "s/var build_time = \"dev\"/var build_time = \"$BUILD_DATE\"/" singletons/build_info.gd
